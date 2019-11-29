fb249f6970bf1f5e456097e21a22cd8e14caa160
karyaTio <karya.tiosaputra@gmail.com>
2019-11-29 11:09:28 +0700

    Article API CRUD

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List Articles
+Route::get('articles', 'ArticleController@index');
+
+// List Single Article
+Route::get('articles/{id}', 'ArticleController@show');
+
+// Create New Article
+Route::post('articles', 'ArticleController@store');
+
+// Update Article
+Route::put('articles', 'ArticleController@store');
+
+// Delete Article
+Route::delete('articles/{id}', 'ArticleController@destroy');
